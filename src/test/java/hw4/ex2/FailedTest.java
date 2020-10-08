f15d10000b1cf47ddf5fff96c9ae8529e6366fac
Nikita Iashin <Nikita_Iashin@epam.com>
2020-10-08 23:26:23 +0300

    add screen shot feature for faulty test

--- src/test/java/hw4/ex2/FailedTest.java
+++ src/test/java/hw4/ex2/FailedTest.java
@@ -1,15 +1,24 @@
 package hw4.ex2;
 
 import hw4.BaseTest;
+import hw4.TestListener;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
 
+
+@Listeners(TestListener.class)
+@Feature("test faulty behavior")
+@Story("return false for openHomePage")
 public class FailedTest extends BaseTest {
 
     //1 open Home Page
     @Test(priority = 1)
     public void openHomePage() {
+        homePage.openHomePage();
         assertTrue(false);
     }
 }
